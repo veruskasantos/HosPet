cf65b3bbdb35049857bf592a07880d18b687b620
veruska <veruska.santos@ccc.ufcg.edu.br>
2017-03-28 16:16:07 -0300

    Adicionando containers

---
+++ HosPet/HosPet/LocalAnfitriaoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LocalAnfitriaoViewController.swift
+//  HosPet
+//
+//  Created by Student on 3/28/17.
+//  Copyright © 2017 Veruska. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocalAnfitriaoViewController: UIViewController, CLLocationManagerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HosPet/HosPet/PerfilAnfitriaoViewController.swift
+++ HosPet/HosPet/PerfilAnfitriaoViewController.swift
@@ -11,38 +11,54 @@ import UIKit
 class PerfilAnfitriaoViewController: UIViewController {
 
     
-    @IBOutlet weak var container01: UIView!
-
-    @IBOutlet weak var container02: UIView!
+    @IBOutlet weak var containerPerfil: UIView!
     
+    @IBOutlet weak var containerAvaliacao: UIView!
     
+    @IBOutlet weak var containerGaleria: UIView!
     
+    @IBOutlet weak var containerLocal: UIView!
+    
+    //Alterando a exibicao dos containers do anfitriao
     @IBAction func opcoesPerfilBar(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
-            container01.isHidden = false
-            container02.isHidden = true
+            containerPerfil.isHidden = false
+            containerAvaliacao.isHidden = true
+            containerGaleria.isHidden = true
+            containerLocal.isHidden = true
         case 1:
             
-            container01.isHidden = true
-            container02.isHidden = false
+            containerPerfil.isHidden = true
+            containerAvaliacao.isHidden = false
+            containerGaleria.isHidden = true
+            containerLocal.isHidden = true
         case 2:
-            break
+            containerPerfil.isHidden = true
+            containerAvaliacao.isHidden = true
+            containerGaleria.isHidden = false
+            containerLocal.isHidden = true
         case 3:
-            break
+            containerPerfil.isHidden = true
+            containerAvaliacao.isHidden = true
+            containerGaleria.isHidden = true
+            containerLocal.isHidden = false
         default:
-            break
+            containerPerfil.isHidden = false
+            containerAvaliacao.isHidden = true
+            containerGaleria.isHidden = true
+            containerLocal.isHidden = true
         }
     }
-    
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        container01.isHidden = false
-        container02.isHidden = true
+        containerPerfil.isHidden = false
+        containerAvaliacao.isHidden = true
+        containerGaleria.isHidden = true
+        containerLocal.isHidden = true
 
         // Do any additional setup after loading the view.
     }
